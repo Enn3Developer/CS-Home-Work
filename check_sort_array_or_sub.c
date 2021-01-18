b41171fc8535fc40fafb592f68caf49628bd621c
Enn3Developer <45467622+Enn3Developer@users.noreply.github.com>
2021-01-18 21:33:55 +0100

    Update check_sort_array_or_sub.c

--- check_sort_array_or_sub.c
+++ check_sort_array_or_sub.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+
+// Given an array, return the aray (or sub-array) that is increment sorted
+
 typedef enum {false, true} bool;
 
 bool is_ordered(int arr[], int start, int end);
@@ -7,26 +10,26 @@ void print_len_ordered(int arr[], int n);
 
 int main() {
     int n;
-    printf("Inserire N: ");
+    printf("How many numbers? ");
     scanf("%d", &n);
     fflush(stdout);
 
     int nums[n];
     for (int i=0; i<n; i++) {
         int input;
-        printf("Inserire un numero [%d/%d]: ", i, n);
+        printf("Write a number [%d/%d]: ", i, n);
         scanf("%d", &input);
         fflush(stdout);
         nums[i] = input;
     }
 
-    printf("Ho rivelato questo sub-array (o array) ordinato in ordine crescente:\n");
+    printf("I discovered this sub-array (or the array) increment sorted:\n");
     print_len_ordered(nums, n);
 
     return 0;
 }
 
-// Raggio: [start, end)
+// Range: [start, end)
 bool is_ordered(int arr[], int start, int end) {
     bool ordered = true;
 
@@ -37,7 +40,7 @@ bool is_ordered(int arr[], int start, int end) {
     return ordered;
 }
 
-// Raggio: [start, end)
+// Range: [start, end)
 void print_array(int arr[], int start, int end) {
     for (int i=start; i<end; i++) {
         printf("%d ", arr[i]);
@@ -53,9 +56,11 @@ void print_len_ordered(int arr[], int n) {
         for (int j=i; j<n; j++) {
             int len_sub = (j - i) + 1;
             int sub_arr[len_sub];
-            for (int k=0; k<len_sub; k++) {
+            for (int k=0; k<len_sub; k++) {  // Create a sub-array from `i` to `j`
                 sub_arr[k] = arr[k+i];
             }
+            // check if the newly created sub-array has at least two elements
+            // if yes then check if that is ordered and his length is bigger than the len of the previous sub-array
             if (len_sub > 1 && is_ordered(sub_arr, 0, len_sub) && len_sub > len) {
                 len = len_sub;
                 for (int k=0; k<len_sub; k++) {
@@ -66,6 +71,6 @@ void print_len_ordered(int arr[], int n) {
     }
 
     if (len > 1) {
-        print_array(sub, 0, len);
+        print_array(sub, 0, len);  // prints only the elements of the sub-array
     }
 }
